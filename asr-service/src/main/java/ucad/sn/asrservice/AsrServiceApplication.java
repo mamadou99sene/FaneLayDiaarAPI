22d21f30bdb20cdaa46a2f610dc53cc34caa2c11
Mamadou SENE <senemamadou1999@gmail.com>
2024-12-05 17:17:16 +0000

    asr

---
+++ asr-service/src/main/java/ucad/sn/asrservice/AsrServiceApplication.java
@@ -0,0 +1,13 @@
+package ucad.sn.asrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsrServiceApplication.class, args);
+    }
+
+}
---
+++ asr-service/src/test/java/ucad/sn/asrservice/AsrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucad.sn.asrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
